1bb710078a68c949f0eeb5adad18f5eeb332e183
AryanRathore04 <aryan4meu@gmail.com>
2024-09-21 18:06:06 +0530

    Added about, Project, Testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Navbar from './sections/Navbar'
 import Hero from './sections/Hero'
+import About from './sections/About'
+import Projects from './sections/Projects'
+import Clients from './sections/Clients'
 
 const App = () => {
   return (
     <main className='max-w-7xl mx-auto'>
       <Navbar />
       <Hero />
+      <About />
+      <Projects />
+      <Clients />
     </main>
   )
 }
